1843eb9b0d1eb8e9abb450558ab1e1de54c9bff4
Vishal Chaurasia <vishalbg.mz@gmail.com>
2023-04-02 19:21:23 +0530

    chapter-1: intro to typescipt

---
+++ chapter-1/build/js/main.js
@@ -0,0 +1,7 @@
+"use strict";
+let myName = 'Vishal';
+console.log(myName);
+let a = 10;
+let b = 2;
+let c = 5;
+console.log(a / b);
---
+++ chapter-1/src/main.ts
@@ -0,0 +1,8 @@
+let myName = 'Vishal';
+console.log(myName);
+
+let a: number = 10;
+let b: number = 2;
+let c: number = 5;
+
+console.log(a / b);
